5e4515e43a9b5ac5026890e239ba8dc60305f08f
Vineet Jadhav <vineet290403@gmail.com>
2024-10-19 19:44:19 +0530

    initial commit

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,7 @@
+import { SendCard } from "../../components/SendCard";
+
+export default function () {
+    return <div className="flex w-screen items-center justify-center">
+        <SendCard />
+    </div>
+}
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,13 @@
-"use client"
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Appbar } from "@repo/ui/appbar";
 
-export default function Page(): JSX.Element {
-  const session = useSession();
-  return (
-   <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
-   </div>
-  );
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/auth";
+
+export default async function Page() {
+  const session = await getServerSession(authOptions)
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
 }
